439cf728b799b2b7fb8901e999ad8e02bc7af06f
lucaMilazzo <l.milazzo1@campus.unimib.it>
2023-03-01 11:20:31 +0100

    updated sampling_CHRR.m

--- code/sampling/sampling_CHRR.m
+++ code/sampling/sampling_CHRR.m
@@ -10,8 +10,7 @@
 %
 %	Prerequisites and parameters:
 %   	- Install the Cobra Toolbox as described at https://opencobra.github.io/cobratoolbox/stable/installation.html
-%		- Fill Line 9 with the path to the installation folder of the Cobra Toolbox (parameter 1)
-%		- FIll Line 16 with the path to your sampling folder (parameter 2)
+%		- Fill Line 16 with the path to the installation folder of the Cobra Toolbox (parameter 1)
 %   
 
 addpath('PATH_TO_COBRA') % parameter 1
@@ -21,12 +20,13 @@ thinnings = {1, 10, 100};
 options.toRound=1;
 for modelNamesIndex=1:length(modelNames)
 	modelName = modelNames{modelNamesIndex}
-	mydir = strcat('PATH_TO_SAMPLES_FOLDER/' , modelName); % parameter 2
+	mydir = strcat('.../samples/' , modelName , '/');
+	mkdir mydir;
 	cd(mydir)
 	if(modelName == "ENGRO1"){
-		model = readCbModel(strcat('../models/',modelName),'fileType','JSON');
+		model = readCbModel(strcat('.../models/',modelName),'fileType','JSON');
 	}else{
-		model = readCbModel(strcat('../models/',modelName),'fileType','SMLB');
+		model = readCbModel(strcat('.../models/',modelName),'fileType','SMLB');
 		listReactions={'EX_O2','EX_Gln','EX_Glc','EX_Arg','EX_THF','EX_Met'};
 		model=changeRxnBounds(model,listReactions,[-38,-40,-10,-20,-20,-20],'b');
 		model=changeRxnBounds(model,listReactions,[1000],'u');
@@ -39,10 +39,8 @@ for modelNamesIndex=1:length(modelNames)
 			options.nStepsPerPoint=thinnings{thinningIndex};
 			options.nPointsReturned=nSample;
 			for h=0:19
-				%Generating samples
 				[modelsampling,samples] = sampleCbModel(model,[], 'CHRR',options); 
 				runtime=toc;
-				%Saving the samples
 				filename=strcat(pwd(),'\',num2str(nSample),'_',num2str(h),'_chrr' ,'.mat');
 				save(filename,'samples');
 			end
